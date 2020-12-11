13f3587a1abda9c093952934fc64698a83fefd9f
georgianastasov <ggeorgianastasov@gmail.com>
2020-12-11 17:17:03 +0200

    Creating classes for transactions
    #24

---
+++ Shop-Store-System/Shop-Store-System/BusinessLogic/transactionDetailBusinessLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Store_System.BusinessLogic
+{
+    class transactionDetailBusinessLogic
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Total { get; set; }
+        public int DealerCustomerId { get; set; }
+        public DateTime AddedDate { get; set; }
+        public int AddedBy { get; set; }
+    }
+}
---
+++ Shop-Store-System/Shop-Store-System/BusinessLogic/transactionsBusinessLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Store_System.BusinessLogic
+{
+    class transactionsBusinessLogic
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int DealerCustomerId { get; set; }
+        public decimal GrandTotal { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Discount { get; set; }
+        public int AddedBy { get; set; }
+        public DataTable TransactionDetails { get; set; }
+    }
+}
